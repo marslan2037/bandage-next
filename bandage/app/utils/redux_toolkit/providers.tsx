616fdb1008b4b97fab4d70336e6bd458c89a9f82
jawad602 <jawadjamil602@gmail.com>
2024-03-28 00:49:51 +0500

    merged

--- bandage/app/utils/redux_toolkit/providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import React from 'react';
-import { Provider } from 'react-redux'; // Adjust the import path to where your store is defined
-import appStore from '@/app/utils/redux_toolkit/appStore';
-
-export function Providers({ children }: { children: React.ReactNode }) {
-    return (<Provider store={appStore}>{children}</Provider>)
-}
